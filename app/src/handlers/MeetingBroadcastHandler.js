7d70d9f0e60e496f84fd0d2438c0de7f2db572d0
Jonathan Wilde <hello@jwilde.me>
2015-03-21 17:56:09 -0400

    Getting the broadcast handler set up.

--- app/src/handlers/MeetingBroadcastHandler.js
+++ app/src/handlers/MeetingBroadcastHandler.js
@@ -2,6 +2,15 @@ var React = require('react');
 
 var MeetingBroadcastHandler = React.createClass({
 
+  statics: {
+    ensureDataAvailable: function(state, flux) {
+      var {meetingId} = state.params,
+          meetingActions = flux.getActions('agenda');
+
+      return meetingActions.fetch(meetingId);
+    }
+  },
+
   render: function() {
     return (
       <div />
