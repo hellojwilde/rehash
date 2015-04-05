c65c02813a0c3a4c63cb1d86fd27ce9fc2b824d9
Jonathan Wilde <hello@jwilde.me>
2015-04-05 16:22:31 -0400

    Rearrange components common to multiple views to be in a common folder. Merge agenda and broadcast. Simplify compound dates. Move modals created with modal system into other folder.

--- client/src/modals/CreateModal.js
+++ client/src/modals/CreateModal.js
@@ -2,11 +2,23 @@ var Modal = require('components/modals/Modal');
 var ModalBody = require('components/modals/ModalBody');
 var ModalHeader = require('components/modals/ModalHeader');
 var ModalFooter = require('components/modals/ModalFooter');
+var {DateInput, TimeInput} = require('react-pick-datetime');
 var React = require('react/addons');
+
+require('react-pick/lib/styles.css');
+require('react-pick-datetime/lib/styles.css');
 
 var LinkedStateMixin = React.addons.LinkedStateMixin;
 
 var moment = require('moment');
+
+function getCompoundDateTime(date, time) {
+  return moment(time).set({
+    date: date.get('date'),
+    month: date.month('month'),
+    year: date.year('year')
+  });
+}
 
 var CreateModal = React.createClass({
 
@@ -29,31 +41,35 @@ var CreateModal = React.createClass({
   },
 
   getInitialState: function() {
+    var start = moment().add(2, 'hours');
+
     return {
       title: '',
       description: '',
-      startDate: '',
-      startTime: '' 
+      startDate: moment(start),
+      startTime: moment(start)
     };
   },
 
   handleCreateClick: function() {
+    var {title, description, startDate, startTime} = this.state;
     var meetingActions = this.context.flux.getActions('meeting');
 
+    // TODO Validate the contents of this somehow.
+
     meetingActions.create({
-      title: this.state.title,
-      description: this.state.description,
-      start: moment(
-        `${this.state.startDate} ${this.state.startTime}`, 
-        'YYYY-MM-DD HH:mm:ss Z'
-      )
+      title: title,
+      description: description,
+      start: getCompoundDateTime(startDate, startTime)
     }).then((meetingId) => this.props.onComplete(meetingId));
   },
 
   render: function() {
     return (
       <Modal>
-        <ModalHeader onCancel={this.props.onCancel}>Create Meeting</ModalHeader>
+        <ModalHeader onCancel={this.props.onCancel}>
+          Create Meeting
+        </ModalHeader>
         <ModalBody>
           <div className="form-group">
             <label htmlFor="CreateModalTitle">Title</label>
@@ -78,24 +94,28 @@ var CreateModal = React.createClass({
             <div className="col-xs-6">
               <div className="form-group">
                 <label htmlFor="CreateModalDate">Start Date</label>
-                <input 
-                  name="CreateModalStartDate"
-                  className="form-control"
-                  type="date"
-                  valueLink={this.linkState('date')}
-                />
+                <div>
+                  <DateInput 
+                    name="CreateModalStartDate"
+                    className="form-control"
+                    value={this.state.startDate}
+                    onChange={(startDate) => this.setState({startDate})}
+                  />
+                </div>
               </div>
             </div>
 
             <div className="col-xs-6">
               <div className="form-group">
                 <label htmlFor="CreateModalTime">Start Time</label>
-                <input 
-                  name="CreateModalStartTime"
-                  className="form-control"
-                  type="time"
-                  valueLink={this.linkState('time')}
-                />
+                <div>
+                  <TimeInput 
+                    name="CreateModalStartTime"
+                    className="form-control"
+                    value={this.state.startTime}
+                    onChange={(startTime) => this.setState({startTime})}
+                  />
+                </div>
               </div>
             </div>
           </div>
