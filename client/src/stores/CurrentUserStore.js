412830caf0fe59f35a6a04a8f3d938dc49e7fb57
Jonathan Wilde <hello@jwilde.me>
2015-04-17 21:35:36 -0400

    Drop most of CurrentUserStore

--- client/src/stores/CurrentUserStore.js
+++ client/src/stores/CurrentUserStore.js
@@ -1,7 +1,5 @@
 var {Store} = require('flummox');
 
-var _ = require('lodash');
-
 class CurrentUserStore extends Store {
   static deserialize(json) {
     return json;
@@ -14,42 +12,10 @@ class CurrentUserStore extends Store {
   constructor(registry) {
     super();
 
-    var meetingActionIds = registry.getActionIds('meeting');
-
-    this.register(meetingActionIds.join, this.handleMeetingJoin);
-    this.register(meetingActionIds.create, this.handleMeetingCreate);
-
     this.state = {
       channelToken: null,
-      user: null,
-      attending: {},
-      hosting: {}
+      user: null
     };
-  }
-
-  getCurrentUser() {
-    return this.state.user;
-  }
-
-  isAttendee(meetingKey) {
-    return _.has(this.state.attending, meetingKey);
-  }
-
-  isHost(meetingKey) {
-    return _.has(this.state.hosting, meetingKey);
-  }
-
-  handleMeetingJoin(meetingKey) {
-    this.setState({
-      attending: _.assign(this.state.attending, {meetingKey: true})
-    });
-  }
-
-  handleMeetingCreate(meetingKey) {
-    this.setState({
-      attending: _.assign(this.state.attending, {meetingKey: true}),
-      hosting: _.assign(this.state.hosting, {meetingKey: true})
-    });
   }
 }
 
