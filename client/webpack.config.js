1d7fec128d740a3202fd207f6c999bcc6395b4ae
Jonathan Wilde <hello@jwilde.me>
2015-04-20 15:33:47 -0400

    Almost done.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 
 module.exports = {
+  devtool: 'inline-source-map',
   entry: [
     'webpack-dev-server/client?http://0.0.0.0:3001',
     'webpack/hot/only-dev-server',
