15eae81533f14cdaf814e3432425ce25a950831f
HaoWan <wanhao.maple@gmail.com>
2015-04-21 13:28:13 -0400

    env var

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 OAUTH_CONFIG = { 'tw': { 
-             'consumer_key': 'c', 
-             'consumer_secret': 'b', 
+             'consumer_key': '', 
+             'consumer_secret': '', 
              'request_token_url': 'https://twitter.com/oauth/request_token', 
              'access_token_url': 'https://twitter.com/oauth/access_token', 
              'user_auth_url': 'http://twitter.com/oauth/authorize', 
              'default_api_prefix': 'http://twitter.com',
              'default_api_suffix': '.json',
              'account_url': 'https://api.twitter.com/1.1/account/verify_credentials.json', 
-             'callback_url': 'a',
+             'callback_url': 'CALLBACK_URL',
            }, 
            'internal': { 
              'logout_redirect_url': '/', 
